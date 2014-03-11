e28a3b9762824687e1bcf6f95165052beda3d145
Matt Kaniaris <mkaniaris@gmail.com>
2014-03-11 15:43:52 -0700

    now iterating through children

--- js-git-ignore.js
+++ js-git-ignore.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
 
+var buildIgnores = require("./lib/ignore"),
+    floorine = require("floorine");
+
 exports.run = function () {
+  floorine.set_log_level("debug");
+  floorine.log("starting");
+  var ignore = buildIgnores(process.cwd(), function(err, res) {
+    floorine.log("all done");
+  });
 };
